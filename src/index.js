f9935ebb9e8e00b0aedcec71fcea8522fb735a62
Sergey Zhmudikov <32043463+SergeyZhmudikov@users.noreply.github.com>
2018-02-26 16:42:29 +0300

    sorter done

--- src/index.js
+++ src/index.js
@@ -1,31 +1,47 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.data = [];
+        this.condition = false;
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.data.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.data[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.data.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.data;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        indices.sort();
+        let arr = [];
+        for (let i = 0; i < indices.length; i++) {
+            arr.push(this.data[indices[i]]);
+        }
+        if (this.condition == true) {
+            arr.sort(this.compareFunction);
+        } else {
+            arr.sort((a, b) => {
+                return a - b;
+            });
+        }
+        for (let i = 0; i < indices.length; i++) {
+            this.data[indices[i]] = arr[i];
+        }
+    }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    setComparator(compareFunction) {
+        this.compareFunction = compareFunction;
+        this.condition = true;
+    }
 }
 
 module.exports = Sorter;
